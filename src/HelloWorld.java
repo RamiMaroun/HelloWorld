06c562d00bfd8173422ae7627ef775a249719bec
RamiM <s351882@oslomet.no>
2021-08-27 14:44:40 +0200

    Rami teller Nå

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,9 @@ public class HelloWorld {
         for(int i=1; i<=10; i++){
             System.out.println(i);
         }
+        System.out.println("Rami Teller nå!");
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
     }
 }
